ac4d1ae38df7c05a487bf784b399ae0ec4ee2134
Maximilian Haack <mxhaack@gmail.com>
2017-05-03 17:44:57 +0800

    Add test for json schemas and resources

---
+++ spec/json_validation_spec.rb
@@ -0,0 +1,40 @@
+describe 'JSON Schema Validation' do
+  context 'JSON Schemas' do
+    let(:draft_schema) do
+      JSON::Validator
+        .validator_for_name('http://json-schema.org/draft-04/schema#')
+        .metaschema
+    end
+
+    Dir[File.expand_path('../../latest/**.json', __FILE__)].each do |schema|
+      it "validates #{schema.slice(schema.index('latest')..-1)}" do
+        expect do
+          JSON::Validator.validate!(draft_schema, schema)
+        end.not_to raise_error
+      end
+    end
+  end
+
+  Dir[File.expand_path('../../test_data/**', __FILE__)].each do |resource_type|
+    context resource_type.slice(resource_type.index('test_data')..-1).to_s do
+      schema_name = resource_type.split('/').last.to_sym
+      resources   = Dir[resource_type + '/**']
+
+      context 'validates resources' do
+        resources.reject { |r| r =~ /invalid/ }.each do |resource|
+          it resource.split('/').last.to_s do
+            expect(resource).to match_json_schema(schema_name)
+          end
+        end
+      end
+
+      context 'invalidates resources' do
+        resources.reject { |r| r !~ /invalid/ }.each do |resource|
+          it resource.split('/').last.to_s do
+            expect(resource).not_to match_json_schema(schema_name)
+          end
+        end
+      end
+    end
+  end
+end
